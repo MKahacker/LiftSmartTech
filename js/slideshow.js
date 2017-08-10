581a6b7dccf7321086ea092c6c22da6896ca5389
Makk-Attack <k-makk@live.com>
2017-08-09 23:40:01 -0500

    Update slideshow.js
    fixing spelling of length

--- js/slideshow.js
+++ js/slideshow.js
@@ -14,9 +14,9 @@ function displaySlide(n){
   var i; 
   var x = document.getElementsByClassName("slide-images");
   if(slideIndex < 1){
-    slideIndex = x.lenght;
+    slideIndex = x.length;
   }
-  if(slideIndex > x.lenght){
+  if(slideIndex > x.length){
     slideIndex = 1;
   }
  /* for(i = 0; i < x.length; i++){
